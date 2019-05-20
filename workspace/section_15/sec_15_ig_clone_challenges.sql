ae5b18051fbeedecd328f45b385d4a138e77f30c
GeneCPChung <genecpc@gmail.com>
2019-05-19 22:23:51 -0400

    added several sections

--- workspace/section_15/sec_15_ig_clone_challenges.sql
+++ workspace/section_15/sec_15_ig_clone_challenges.sql
@@ -3,6 +3,7 @@ SELECT *
 FROM users
 ORDER BY created_at ASC LIMIT 5;
 
+
 --WHAT DAY OF THE WEEK DO MOST USERS REGISTER ON
 SELECT
     DAYNAME(created_at) AS day,
@@ -14,11 +15,82 @@ LIMIT 2;
 
 
 --IDENTIFY INACTIVE USERS
-SELECT *
+SELECT username
 FROM users
 LEFT JOIN photos
     ON users.id = photos.user_id
 WHERE photos.id IS NULL;
+
+
+--WHO GOT THE MOST LIKES
+SELECT 
+    username,
+    photos.id,
+    photos.image_url, 
+    COUNT(*) AS total
+FROM photos
+INNER JOIN likes
+    ON likes.photo_id = photos.id
+INNER JOIN users
+    ON photos.user_id = users.id
+GROUP BY photos.id
+ORDER BY total DESC
+LIMIT 1;
+
+
+--AVERAGE USER POSTINGS
+SELECT (SELECT COUNT(*) FROM photos) / (SELECT COUNT(*) FROM users) AS Avg;
+    
+
+--TOP 5 MOST USED HASHTAGS
+SELECT 
+    tags.tag_name,
+    COUNT(*) AS total
+FROM photo_tags
+JOIN tags
+    ON photo_tags.tag_id = tags.id
+GROUP BY tags.id
+ORDER BY total DESC
+LIMIT 5;
+
+--FINDING BOTS
+
+SELECT 
+    username,
+    COUNT(*) as num_likes
+FROM users
+INNER JOIN likes
+    ON users.id = likes.user_id
+GROUP BY likes.user_id
+HAVING num_likes = (SELECT COUNT(*) FROM photos);
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
